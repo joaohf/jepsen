b84551c6c293d0107724fccf960ae5094f7e89e3
João Henrique Ferreira de Freitas <joaohf@gmail.com>
2016-05-04 18:24:50 -0300

    Deps update

--- elasticsearch/project.clj
+++ elasticsearch/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-http "1.1.0"]
-                 [jepsen "0.0.4-SNAPSHOT"]
-                 [clojurewerkz/elastisch "2.2.0-beta2"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "2.1.0"]
+                 [jepsen "0.1.1-SNAPSHOT"]
+                 [clojurewerkz/elastisch "2.2.1"]])  
